47b840ef74fa6668b5dc25efb138e322ae8c6580
Hamayoun <hamayounemail@gmail.com>
2022-10-04 17:38:28 +0430

    Fixed linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,6 +1,6 @@
 require_relative 'base_decorator'
 
-class Trimmer_decorator < Base_decorator
+class TrimmerDecorator < BaseDecorator
   def correct_name
     @nameable.slice(0, 10) if name.size > 10
   end
